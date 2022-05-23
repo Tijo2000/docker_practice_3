11201ad626f1944a85b2ea124c472752cfe15dd7
Tijo2000 <56153083+Tijo2000@users.noreply.github.com>
2022-05-23 15:37:57 +0530

    first commit

---
+++ docker_practice_3/main.py
@@ -0,0 +1,28 @@
+# Simple pygame program
+
+# Import and initialize the pygame library
+import pygame
+pygame.init()
+
+# Set up the drawing window
+screen = pygame.display.set_mode([500, 500])
+
+# Running the game until the user asks to quit
+running = True
+while running:
+
+    # creating an if statement for the close button
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Filling the background color with a gray
+    screen.fill((55, 55, 55))
+
+    # Drawing a solid purple circle in the center
+    pygame.draw.circle(screen, (0, 0, 251), (251, 251), 74)
+    # Flip the display
+    pygame.display.flip()
+
+# Done! Time to quit.
+pygame.quit()
